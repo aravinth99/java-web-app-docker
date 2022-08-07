8c392261f2398a4ae1242aa21c5446dbfdd74db0
aravinth99 <93654046+aravinth99@users.noreply.github.com>
2022-08-07 11:05:30 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Ram";
+			return "Hello Aravinth";
 		}else{
 			return "Hello " + name;
 		}
